a7c68bd81edae0efacda61258bbe29e0013a2a72
nouraboussaoud <nour.aboussaoud@esprit.tn>
2025-04-12 23:14:56 +0100

    Premières modifications les logs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductsComponent } from './products/products.component';
 import { FormsModule } from '@angular/forms';
-import { NgbModal, NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+
+import { ProductsComponent } from './products/products.component';
 import { StockComponent } from './stock/stock.component';
 import { ReglementComponent } from './reglement/reglement.component';
 import { SecteurActiviteComponent } from './secteur-activite/secteur-activite.component';
 import { OperateurComponent } from './operateur/operateur.component';
 import { FactureComponent } from './facture/facture.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger'; // ✅ Ajout ngx-logger
 
 @NgModule({
   declarations: [
@@ -33,7 +35,12 @@ import { NavbarComponent } from './navbar/navbar.component';
     FormsModule,
     NgbModalModule,
     RouterModule,
-    NgbModule
+    NgbModule,
+    LoggerModule.forRoot({                           // ✅ Configuration du logger
+      level: NgxLoggerLevel.DEBUG,
+      serverLogLevel: NgxLoggerLevel.ERROR,
+      serverLoggingUrl: 'http://localhost:8080/api/logs' // Tu peux le créer plus tard
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {Product} from '../shared/Model/Product';
-import {ProductService} from '../shared/Service/Product.service';
+import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Product } from '../shared/Model/Product';
+import { ProductService } from '../shared/Service/Product.service';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-products',
@@ -15,12 +16,15 @@ export class ProductsComponent implements OnInit {
   product!: Product;
   closeResult!: string;
 
-  constructor(private productService: ProductService, private modalService: NgbModal) {
-  }
+  constructor(
+    private productService: ProductService,
+    private modalService: NgbModal,
+    private logger: NGXLogger
+  ) {}
 
   ngOnInit(): void {
+    this.logger.info('Initialisation du composant ProductsComponent');
     this.getAllProducts();
-
     this.product = {
       idProduit: null,
       codeProduit: null,
@@ -28,52 +32,72 @@ export class ProductsComponent implements OnInit {
       prix: null,
       dateCreation: null,
       dateDerniereModification: null
-
-    }
+    };
   }
 
   getAllProducts() {
-    this.productService.getAllProducts().subscribe(res => this.listProducts = res)
+    this.logger.debug('Chargement de tous les produits...');
+    this.productService.getAllProducts().subscribe(res => {
+      this.logger.info('Produits récupérés avec succès');
+      this.listProducts = res;
+    });
   }
 
   addProduct(p: any) {
+    this.logger.debug('Ajout d’un nouveau produit', p);
     this.productService.addProduct(p).subscribe(() => {
+      this.logger.info('Produit ajouté avec succès');
       this.getAllProducts();
       this.form = false;
     });
   }
 
   editProduct(product: Product) {
-    this.productService.editProduct(product).subscribe();
+    this.logger.debug('Modification du produit', product);
+    this.productService.editProduct(product).subscribe(() => {
+      this.logger.info(`Produit modifié: ${product.idProduit}`);
+    });
   }
 
   deleteProduct(idProduct: any) {
-    this.productService.deleteProduct(idProduct).subscribe(() => this.getAllProducts())
+    this.logger.warn(`Suppression du produit ID=${idProduct}`);
+    this.productService.deleteProduct(idProduct).subscribe(() => {
+      this.logger.info(`Produit supprimé: ID=${idProduct}`);
+      this.getAllProducts();
+    });
   }
 
   open(content: any, action: any) {
-    if (action != null)
-      this.product = action
-    else
+    this.logger.debug('Ouverture du modal');
+    if (action != null) {
+      this.product = action;
+      this.logger.debug('Produit sélectionné pour modification', action);
+    } else {
       this.product = new Product();
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.logger.debug('Création d’un nouveau produit');
+    }
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
+      this.logger.debug(`Modal fermé avec: ${result}`);
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      this.logger.debug(`Modal fermé (raison): ${this.closeResult}`);
     });
   }
 
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
+      return 'en appuyant sur ÉCHAP';
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
+      return 'en cliquant sur l’arrière-plan';
     } else {
-      return `with: ${reason}`;
+      return `avec: ${reason}`;
     }
   }
 
   cancel() {
     this.form = false;
+    this.logger.info('Annulation de l’action, formulaire masqué');
   }
 }
